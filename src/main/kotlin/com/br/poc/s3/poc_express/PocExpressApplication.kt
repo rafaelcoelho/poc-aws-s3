1d04dfe39f880902c321a387a0d0979c3c7de152
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2025-01-30 15:26:13 -0300

    Initial commit

---
+++ src/main/kotlin/com/br/poc/s3/poc_express/PocExpressApplication.kt
@@ -0,0 +1,11 @@
+package com.br.poc.s3.poc_express
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocExpressApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocExpressApplication>(*args)
+}
