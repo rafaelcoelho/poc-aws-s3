2d903723ddb01751919a31f78ca011eaa62286e4
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2025-01-30 16:08:53 -0300

    Add base deps in order to consume aws service

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,26 @@ repositories {
 }
 
 dependencies {
+	implementation(platform("software.amazon.awssdk:bom:2.29.46"))
+	implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.2.1"))
+
+	implementation("software.amazon.awssdk:sts")
+	implementation("software.amazon.awssdk:http-auth-aws")
+	implementation("io.awspring.cloud:spring-cloud-aws-starter")
+	implementation("io.awspring.cloud:spring-cloud-aws-starter-s3")
+	implementation("io.awspring.cloud:spring-cloud-aws-starter-secrets-manager")
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
